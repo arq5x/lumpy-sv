a4eee992cfe368b9323c919197e9d929c1858ad7
Ryan Layer <ryan.layer@gmail.com>
2013-01-15 10:18:51 -0500

    test counts

--- src/lumpy/lumpy.cpp
+++ src/lumpy/lumpy.cpp
@@ -520,6 +520,8 @@ int main(int argc, char* argv[])
 			}
 			//}}}
 
+			cerr << "bps:" << r_bin.num_bps() << "\t";
+
 			//{{{ get breakpoints
 			vector< UCSCElement<SV_BreakPoint*> > values = 
 					r_bin.values(min_chr, max_pos);
@@ -552,7 +554,7 @@ int main(int argc, char* argv[])
 			}
 			//}}}
 
-			//cerr << r_bin.num_bps() << endl;
+			cerr << r_bin.num_bps() << endl;
 			max_pos = max_pos *2;
 		}
 
