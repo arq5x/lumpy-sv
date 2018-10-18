483674856dcc400dde55b729f185019188f2c411
hepcat72 <hepcat72@gmail.com>
2018-10-18 17:33:18 -0400

    The call to lumpy from lumpyexpress segfaults when the -pe flags are omitted when the insert mean and stdev are not calculated in lumpyexpress. Furthermore, the lumpy galaxy wrapper fails with an error that mean & stdev are missing.
    I kept the warning, but allowed mean and stdev to be output in all cases to avoid the segfaults from lumpyexpress and avoid the error & no output from the lumpy galaxy wrapper.
    
    This is admittedly a work-around. Lumpy should probably be edited to be able to not segfault when the -pe discordant options are missing and should gracefully output no results if mean and stdev do not have valid values.
    
    Files checked in: scripts/lumpyexpress scripts/pairend_distro.py

--- scripts/pairend_distro.py
+++ scripts/pairend_distro.py
@@ -112,41 +112,38 @@ for l in sys.stdin:
 min_elements = 1000
 if len(L) < min_elements:
     sys.stderr.write("Warning: only %s elements in distribution (min: %s)\n" % (len(L), min_elements))
-    mean = "NA"
-    stdev = "NA"
 
-else:
-    # Remove outliers
-    med, umad = unscaled_upper_mad(L)
-    upper_cutoff = med + options.mads * umad
-    L = [v for v in L if v < upper_cutoff]
-    new_len = len(L)
-    removed = c - new_len
-    sys.stderr.write("Removed %d outliers with isize >= %d\n" %
-        (removed, upper_cutoff))
-    c = new_len
+# Remove outliers
+med, umad = unscaled_upper_mad(L)
+upper_cutoff = med + options.mads * umad
+L = [v for v in L if v < upper_cutoff]
+new_len = len(L)
+removed = c - new_len
+sys.stderr.write("Removed %d outliers with isize >= %d\n" %
+    (removed, upper_cutoff))
+c = new_len
 
-    mean, stdev = mean_std(L)
+mean, stdev = mean_std(L)
 
-    start = options.read_length
-    end = int(mean + options.X*stdev)
+start = options.read_length
+end = int(mean + options.X*stdev)
 
-    H = [0] * (end - start + 1)
-    s = 0
+H = [0] * (end - start + 1)
+s = 0
 
-    for x in L:
-        if (x >= start) and (x <= end):
-            j = int(x - start)
-            H[j] = H[ int(x - start) ] + 1
-            s += 1
+for x in L:
+    if (x >= start) and (x <= end):
+        j = int(x - start)
+        H[j] = H[ int(x - start) ] + 1
+        s += 1
 
-    f = open(options.output_file, 'w')
+f = open(options.output_file, 'w')
 
-    for i in range(end - start):
-        o = str(i) + "\t" + str(float(H[i])/float(s)) + "\n"
-        f.write(o)
+for i in range(end - start):
+    o = str(i) + "\t" + str(float(H[i])/float(s)) + "\n"
+    f.write(o)
 
 
-    f.close()
+f.close()
 
 print(('mean:' + str(mean) + '\tstdev:' + str(stdev)))
